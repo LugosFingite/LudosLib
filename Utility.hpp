9bd9ab70bd45facfd0e489a43a6bdcd2d0018ed8
FlambyProgrammer <faremy03@gmail.com>
2017-12-06 13:54:07 +0100

    Removed useless comments

--- Utility.hpp
+++ Utility.hpp
@@ -4,23 +4,23 @@
 namespace LudosLib
 {
 	template <typename T>
-	void swap(T& a, T& b)								//Swaps 2 values
+	void swap(T& a, T& b)
 	{
 		T temp = a;
 		a = b; b = temp;
 	}
 
 	template <typename T>
-	void copy(T* object, const T* original, size_t copyLength)			//Copies the content of list B to list A
+	void copy(T* object, const T* original, size_t copyLength)
 	{
 		for (size_t i = 0; i < copyLength; i++)
 			object[i] = original[i];
 	}
 
 	template <typename T>
-	void reverse(T* object, size_t size)						//Reverses a list
+	void reverse(T* object, size_t size)
 	{
-		for (size_t i = 0; i < size--; i++) //Ugly ass for... x)
+		for (size_t i = 0; i < size--; i++)
 			swap(object[i], object[size]);
 	}
 }
